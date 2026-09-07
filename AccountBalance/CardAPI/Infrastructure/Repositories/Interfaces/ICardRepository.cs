6ce8e3d755ecde342fba16e8186a27d48df47b73
Victor <00143320@uca.edu.sv>
2026-09-07 01:16:44 -0600

    feature: Created repositories interfaces for Card and Client 🚧

---
+++ AccountBalance/CardAPI/Infrastructure/Repositories/Interfaces/ICardRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace CardAPI.Infrastructure.Repositories.Interfaces
+{
+    public interface ICardRepository
+    {
+        public Task<object> GetCardAsync(string cardId);
+        public Task<object> GetCardTransaccionAsync(string cardId);
+        public Task<object> GetCardBalanceAsync(string cardId);
+        public Task<object> AddCardPurchase(string cardId, decimal amount);
+        public Task<object> AddCardPayment(string cardId, decimal amount);
+
+    }
+}
---
+++ AccountBalance/CardAPI/Infrastructure/Repositories/Interfaces/IClientRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace CardAPI.Infrastructure.Repositories.Interfaces
+{
+    public interface IClientRepository
+    {
+        public Task<object> GetUserAndCards(string user);
+        public Task<object> GetUserPurchases(int userId, int cardId);
+    }
+}
