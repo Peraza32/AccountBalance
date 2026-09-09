c41c9e12af04cd5f47d2c31fe9c32b63bb499ad0
Victor <00143320@uca.edu.sv>
2026-09-09 10:11:31 -0600

    feature: Automapper profiles

---
+++ AccountBalance/CardAPI/Utils/Profiles/PaymentProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CardAPI.Application.Card.Command.Payment;
+using CardAPI.Domain.Entities.DTO;
+using CardAPI.Domain.Models;
+
+namespace CardAPI.Utils.Profiles
+{
+    public class PaymentProfile : Profile
+    {
+        public PaymentProfile()
+        {
+            CreateMap<addPaymentCommand, PaymentsTc>()
+                .ForMember(dest => dest.IdCard, opt => opt.MapFrom(src => src.cardId))
+                .ForMember(dest => dest.MvDate, opt => opt.MapFrom(src => src.paymentDate))
+                .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.amount))
+                .ForMember(dest => dest.MvDescription, opt => opt.MapFrom(src => src.description));
+
+            CreateMap<CardPaymentDTO, addPaymentCommand>();
+        }
+    }
+}
---
+++ AccountBalance/CardAPI/Utils/Profiles/PurchaseProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CardAPI.Application.Card.Command.Purchase;
+using CardAPI.Domain.Entities.DTO;
+using CardAPI.Domain.Models;
+
+namespace CardAPI.Utils.Profiles
+{
+    public class PurchaseProfile:Profile
+    {
+        public PurchaseProfile() {
+            CreateMap<AddPurchaseCommand, MovementsTc>()
+                .ForMember(dest => dest.IdCard, opt => opt.MapFrom(src => src.cardId))
+                .ForMember(dest => dest.MvDate, opt => opt.MapFrom(src => src.purchaseDate))
+                .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.price))
+                .ForMember(dest => dest.MvDescription, opt => opt.MapFrom(src => src.description));
+
+            CreateMap<PurchaseDTO, AddPurchaseCommand>();
+        }
+    }
+}
